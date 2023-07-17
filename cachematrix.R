aa8dbc80ba3dd4b8c7fe1385e5359e800e2d3633
Pankaj Sahu <62869103+PankajSahu-DEV@users.noreply.github.com>
2023-07-17 22:24:47 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,28 +1,44 @@
-makeCacheMatrix <- function(x = matrix()) {
 
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
+makeCacheMatrix <- function(x) {
+
+  cache <- NULL
+  
+  
+  set <- function(matrix) {
+    x <<- matrix 
+    cache$inverse <<- NULL 
   }
-  get <- function() x
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+  
+
+  get <- function() {
+    x  
+  }
+  
+  
+  getInverse <- function() {
+    
+    if (!is.null(cache$inverse)) {
+      message("Getting cached inverse")
+      return(cache$inverse)
+    }
+    
+    
+    message("Calculating inverse")
+    cache$inverse <- solve(x)
+    cache$inverse
+  }
+  
+
+  list(set = set, get = get, getInverse = getInverse)
 }
 
 
+cacheSolve <- function(x) {
 
-cacheSolve <- function(x, ...) {
-  
-  if(!is.null(inv)) {
-    message("getting cached matrix inverse")
-    return(inv)
+  if (!is.function(x$getInverse)) {
+    stop("Input is not a valid cache matrix object.")
   }
-  data <- x$get()
-  inv <- solve(data, ...)
-  x$setinverse(inv)
-  inv
+  
+ 
+  x$getInverse()
 }
